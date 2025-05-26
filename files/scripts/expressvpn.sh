03ad43f372bd43ac0aaef94f6f12b55088161ad4
anhminhng20 <minhanh20720@gmail.com>
2025-05-26 16:51:36 +0700

    Add expressvpn

---
+++ files/scripts/expressvpn.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+# Setup repo
+cat <<EOF >/etc/yum.repos.d/expressvpn.repo
+[expressvpn]
+name=Expressvpn
+baseurl=https://repo.expressvpn.com/public/rpm/any-distro/any-version/\$basearch
+gpgkey=https://www.expressvpn.com/expressvpn_release_public_key_0xAFF2A1415F6A3A38.asc
+gpgcheck=0
+repo_gpgcheck=0
+enabled=1
+type=rpm-md
+EOF
+
+rpm --import https://www.expressvpn.com/expressvpn_release_public_key_0xAFF2A1415F6A3A38.asc
+
+rpm-ostree install expressvpn
+
+rm -f /etc/yum.repos.d/expressvpn.repo
